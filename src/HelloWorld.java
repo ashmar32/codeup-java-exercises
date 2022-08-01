932a0e963936e47a5a3957c60da55d02e35e3937
Ashley Martinez <ashley.martinez32@gmail.com>
2022-08-01 15:14:46 -0500

    refactored exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,12 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
+//        println will return a new line *even if blank .println();
         System.out.println("Hello, World!");
         System.out.print("Hello, World!");
         System.out.print("Lola!");
     }
 }
-////public class Main {
-//    public static void main(String[] args) {
-//        System.out.println("Hello, World!");
-//    }
-//}
+/*public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}*/
