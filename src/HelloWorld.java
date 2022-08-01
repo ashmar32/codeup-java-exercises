ece6738520dc6c4086935e35d80c40fab10edda7
Ashley Martinez <ashley.martinez32@gmail.com>
2022-08-01 16:50:39 -0500

    syntax website excerise 13

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,13 +1,57 @@
+//public class HelloWorld {
+//    public static void main(String[] args) {
+////        println will return a new line *even if blank .println();
+//        System.out.println("Hello, World!");
+//        System.out.print("Hello, World!");
+//        System.out.print("Lola!");
+//    }
+//}
 public class HelloWorld {
     public static void main(String[] args) {
-//        println will return a new line *even if blank .println();
-        System.out.println("Hello, World!");
-        System.out.print("Hello, World!");
-        System.out.print("Lola!");
+//Create an int variable named myFavoriteNumber and assign your favorite number to it,
+        int myFavoriteNumber = 32;
+//then print it out to the console.
+        System.out.println(myFavoriteNumber);
+//2. Create a String variable named myString and assign a string value to it
+        String myString = "This is my string";
+//3. Change your code to assign a character value to myString. What do you notice; the error string can't be converted to char
+//char myCharString = (char) myString;
+
+//4. Change your code to assign the value 3.14159 to myString. What happens? This will cause an error due to a double being provided, but a string is required.
+//        String myString = 3.14159;
+
+//then print the variable out to the console.
+        System.out.println(myString);
+//5. Declare a long variable named myNumber, but do not assign anything to it.
+//      long myNumber = ;
+//Next try to print out myNumber to the console. What happens? This doesn't work because nothing was initialized since it was left blank
+//      System.out.println(myNumber);
+// 6. Change your code to assign the value 3.14 to myNumber. What do you notice? error due to providing a double number and not a long
+//       long myNumber = 3.14;
+//7. Change your code to assign the value 123L (Note the 'L' at the end) to myNumber.
+//        long myNumber = 123L;
+//        System.out.println(myNumber);
+//8. Change your code to assign the value 123 to myNumber.
+        long myNumber = 123;
+//        System.out.println(myNumber);
+//9. Change your code to declare myNumber as a float. Assign the value 3.14 to it.
+//         float myNumber = 3.14;
+// What happens? What are two ways we could fix this? assign a new variable name and use double
+//10. execute the code below
+// this block
+//        int x = 5;
+//will output 5 first because the x is before the increment
+//        System.out.println(x++);
+// will output 6 because the increment has happened in the line above and now x is 6
+//        System.out.println(x);
+
+//        int x = 5;
+// this will output 6 first because the increment happens before the x
+//        System.out.println(++x);
+// this will still output 6 because the increment happened prior to the x
+//        System.out.println(x);
+//11. Try to create a variable named class. What happens?it doesn't work because it's a reserved word
+//        string class = "will this work?"
+//12. Object is the most generic type in Java. You can assign any value to a variable of type Object. What do you think will happen when the following code is run?
     }
 }
-/*public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}*/
