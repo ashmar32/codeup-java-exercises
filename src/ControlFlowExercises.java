42a60b75b22b54d2c9a428506209a6f5778325e0
Ashley Martinez <ashley.martinez32@gmail.com>
2022-08-02 14:51:25 -0500

    completed prob1

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,10 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+//        1. LOOP BASICS
+        int i = 5;
+        while (i <= 15) {
+            System.out.println("i is " + i);
+            i++;
+        }
+    };
+}
