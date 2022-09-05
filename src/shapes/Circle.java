ee00af2ddcf20e598ca4dbb6c8da794dc634b9d4
Ashley Martinez <ashley.martinez32@gmail.com>
2022-09-04 23:01:07 -0500

    completed circles

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -11,8 +11,16 @@ public class Circle {
     public double getArea() {
         return Math.PI * Math.pow(this.radius, 2);
     }
-    public void setRadius() {
 
+    public double getCircumference() {
+        return 2 * Math.PI * this.radius;
     }
 
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
 }
